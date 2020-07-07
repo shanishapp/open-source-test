34eaf14f6115e4f2a2e6bdb118753851f29d4c1d
shani shapp <shanishapp@gmail.com>
2020-07-07 09:54:53 +0300

    final result

--- app/src/main/java/com/example/trypicasso/MainActivity.java
+++ app/src/main/java/com/example/trypicasso/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.trypicasso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -13,8 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.urlInput);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = findViewById(R.id.image_view);
+                Picasso.get().load(editText.getText().toString()).resize(800,600).into(imageView);
+            }
+        });
 
-        ImageView imageView = findViewById(R.id.image_view);
-        Picasso.get().load("https://newevolutiondesigns.com/images/freebies/cool-wallpaper-1.jpg").into(imageView);
     }
 }
